4f63804b3cb3e2fa18f7a1c19cb24395ecd9b09a
Daniel Meinicke <dnlfg.contato@gmail.com>
2025-02-22 18:20:26 -0300

    improved Colors utility

--- src/main/java/com/jlogm/utils/Colors.java
+++ src/main/java/com/jlogm/utils/Colors.java
@@ -12,11 +12,11 @@ public final class Colors {
     public static @NotNull String colored(@NotNull Color color) {
         return "\033[38;2;" + color.getRed() + ";" + color.getGreen() + ";" + color.getBlue() + "m";
     }
-    public static @NotNull String colored(@NotNull Color color, @NotNull String text) {
-        return colored(color) + text + reset();
+    public static @NotNull String colored(@NotNull Color color, @NotNull Object object) {
+        return colored(color) + object + reset();
     }
 
-    public static @NotNull String colored(@Nullable Color foregroundColor, @NotNull Color backgroundColor, @NotNull String text) {
+    public static @NotNull String colored(@Nullable Color foregroundColor, @NotNull Color backgroundColor, @NotNull Object object) {
         if (foregroundColor == null) {
             double luminance = (0.299 * backgroundColor.getRed() + 0.587 * backgroundColor.getGreen() + 0.114 * backgroundColor.getBlue()) / 255;
             foregroundColor = luminance > 0.5 ? Color.BLACK : Color.WHITE;
@@ -24,24 +24,24 @@ public final class Colors {
 
         return colored(foregroundColor) +
                 "m\033[48;2;" + backgroundColor.getRed() + ";" + backgroundColor.getGreen() + ";" + backgroundColor.getBlue() +
-                "m" + text + reset();
+                "m" + object + reset();
     }
 
-    public static @NotNull String underlined(@NotNull String text) {
-        return underlined() + text + reset();
+    public static @NotNull String underlined(@NotNull Object object) {
+        return underlined() + object + reset();
     }
     public static @NotNull String underlined() {
         return "\033[4m";
     }
 
-    public static @NotNull String bold(@NotNull String text) {
-        return "\033[1m" + text + reset();
+    public static @NotNull String bold(@NotNull Object object) {
+        return "\033[1m" + object + reset();
     }
-    public static @NotNull String italic(@NotNull String text) {
-        return "\033[3m" + text + reset();
+    public static @NotNull String italic(@NotNull Object object) {
+        return "\033[3m" + object + reset();
     }
-    public static @NotNull String strikethrough(@NotNull String text) {
-        return "\033[9m" + text + reset();
+    public static @NotNull String strikethrough(@NotNull Object object) {
+        return "\033[9m" + object + reset();
     }
     public static @NotNull String reset() {
         return "\033[0m";
